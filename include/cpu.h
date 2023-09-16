f1a3800c174cae12bd23a12c745317ba3d1c0702
Elmira <89019222+NalbandyanElmira@users.noreply.github.com>
2023-09-16 13:51:02 +0400

    Update cpu.h

--- include/cpu.h
+++ include/cpu.h
@@ -15,18 +15,26 @@
 class CPU
 {
 public:
-	CPU();
-	void fetch(const std::string&);
-	void execute();
-	void printLabels();
+    // Constructor for the CPU class
+    CPU();
+    
+    // Function to fetch instructions from a file
+    void fetch(const std::string&);
+    
+    // Function to execute the fetched instructions
+    void execute();
+    
+    // Function to print the contents of the labels map
+    void printLabels();
+    
 private:
-	Instructions instructions;
-	std::unordered_map<std::string, int> fetched;
-	std::unordered_map<std::string, int> labels;
-	std::unordered_map<std::string, std::function<void(const std::string&, const std::string&)>> functions;
-	std::unordered_map<std::string, std::function<int(int)>> functions2;
-	std::string trim(const std::string&);
-	int pc;
+    Instructions instructions;
+    std::unordered_map<std::string, int> fetched; // Map to store fetched instructions with their line numbers
+    std::unordered_map<std::string, int> labels;  // Map to store labels and their corresponding instruction pointers
+    std::unordered_map<std::string, std::function<void(const std::string&, const std::string&)>> functions; // Map of function pointers for arithmetic and logical instructions
+    std::unordered_map<std::string, std::function<int(int)>> functions2; // Map of function pointers for jump instructions
+    std::string trim(const std::string&); // Function to trim leading and trailing spaces from a string
+    int pc; // Program counter to keep track of the current instruction
 };
 
-#endif //CPU_H
+#endif // CPU_H
